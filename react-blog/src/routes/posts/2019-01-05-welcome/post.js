3dd6d40b065c5bad51a754045a0a152a81834278
morris754 <morris754@gmail.com>
2020-04-12 21:35:14 -0400

    first commit

---
+++ react-blog/src/routes/posts/2019-01-05-welcome/post.js
@@ -0,0 +1,8 @@
+export default {
+  title: `Congratulations on Your New Blog!`,
+  tags: ['react', 'navi'],
+  spoiler: "Learn how to add new posts and tweak the theme to your liking.",
+  getContent: () => import('./test.js'),
+}
+
+
---
+++ react-blog/src/routes/posts/2019-01-05-welcome/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <div>Bitch</div>
+    </div>
+  );
+}
+
+export default App;
